b11c9c79cb5e001e0fa3d65d75c3f4c9bdf57a03
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-08-25 10:34:16 +0100

    add cron job to automate downloading new memes

---
+++ src/workers.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const fetchAndStoreMemeTemplates = async (_args: any, context: any) => {
+  console.log(">>> Fetch and store meme templates cron starting <<<");
+  try {
+    const response = await axios.get("https://api.imgflip.com/get_memes");
+    const promises = response.data.data.memes.map((meme: any) => {
+      return context.entities.Template.upsert({
+        where: { id: meme.id },
+        create: {
+          id: meme.id,
+          name: meme.name,
+          url: meme.url,
+          width: meme.width,
+          height: meme.height,
+          boxCount: meme.box_count,
+        },
+        update: {},
+      });
+    });
+    await Promise.all(promises);
+  } catch (e: unknown) {
+    console.error("Error fetching meme templates:", e);
+  }
+};
